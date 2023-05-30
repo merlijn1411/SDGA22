dad384f0985d13597ccec23f1b2b8616134a8a9b
merlijn1411 <34530@ma-web.nl>
2023-05-30 15:04:09 +0200

    added: Start menu, folders Builds for exe files and package exports, and many scripts

---
+++ IntergalecticHandyman/Assets/script/UI/MenuScreen/GameplaySettings.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+public class GameplaySettings : MonoBehaviour
+{
+
+    //Gameplay Settings
+    [SerializeField] private TMP_Text ControllerTextValue = null;
+    [SerializeField] private Slider controllerSenSlider = null;
+    public int mainControllerSen = 4;
+
+    //toggle Settings
+    [SerializeField] private Toggle invertYToggle = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetControllorSen(float sensitiviyy)
+    {
+        mainControllerSen = Mathf.RoundToInt(sensitiviyy);
+        ControllerTextValue.text = sensitiviyy.ToString("0");
+    }
+
+    public void GameplayApply()
+    {
+        if (invertYToggle.isOn)
+        {
+            PlayerPrefs.SetInt("masterInvertY", 1);
+            //invert y
+        }
+        else
+        {
+            PlayerPrefs.SetInt("masterInvertY", 0);
+            //not invert 
+        }
+
+        PlayerPrefs.SetFloat("masterSen", mainControllerSen);
+        
+    }
+}
---
+++ IntergalecticHandyman/Assets/script/UI/MenuScreen/PathNote.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNote : MonoBehaviour
+{
+    public GameObject PatchMenuUI;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void PatchNote()
+    {
+        PatchMenuUI.SetActive(true);
+    }
+}
