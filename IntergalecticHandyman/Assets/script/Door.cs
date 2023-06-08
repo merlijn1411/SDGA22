f12fad582b61af5bc73cf0cfc340559bdccae831
merlijn1411 <34530@ma-web.nl>
2023-06-08 09:30:56 +0200

    automatic doors added

---
+++ IntergalecticHandyman/Assets/script/Door.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isAuto;
+    public Animator ani;
+    private bool canBeOpened;
+
+    private void OnMouseDown()
+    {
+        if (canBeOpened)
+        {
+            ani.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && isAuto)
+        {
+            ani.SetBool("Open", true);
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !isAuto)
+        {
+            canBeOpened = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            ani.SetBool("Open", false);
+            canBeOpened = false;
+        }
+    }
+}
