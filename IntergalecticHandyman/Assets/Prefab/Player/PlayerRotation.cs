f970c86cda65c864255c0d264e98c8b6d126ba83
merlijn1411 <114576658+merlijn1411@users.noreply.github.com>
2023-06-20 20:32:26 +0200

    added player and modefied room with collider

---
+++ IntergalecticHandyman/Assets/Prefab/Player/PlayerRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float rotationSpeed ;
+
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        transform.Rotate(0, input.x * rotationSpeed, 0);
+
+        playerCamera.transform.Rotate(-input.y * rotationSpeed, 0, 0);
+
+    }
+}
