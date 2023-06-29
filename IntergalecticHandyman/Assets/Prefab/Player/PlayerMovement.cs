1258edd7747c72e73d4b817e6083fe47e35f8e65
Davey <36044@ma-web.nl>
2023-06-29 09:50:13 +0200

    new scene

--- IntergalecticHandyman/Assets/Prefab/Player/PlayerMovement.cs
+++ IntergalecticHandyman/Assets/Prefab/Player/PlayerMovement.cs
@@ -1,36 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed ;
 
-    private AudioSource Audio;
-
-    private void Start()
-    {
-        
-    }
-
+    
     void Update()
     {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         transform.Translate(input * Time.deltaTime * movementSpeed);
-
-        StartCoroutine(LoopAudio());
-
-    }
-    IEnumerator LoopAudio()
-    {
-        Audio = GetComponent<AudioSource>();
-        float length = Audio.clip.length;
-
-        while (true)
-        {
-            Audio.Play();
-            yield return new WaitForSeconds(length);
-        }
     }
 }
--- IntergalecticHandyman/Assets/Prefab/trashGame/showRadius.cs
+++ IntergalecticHandyman/Assets/Prefab/trashGame/showRadius.cs
@@ -37,9 +37,9 @@ public class showRadius : MonoBehaviour
                 Debug.Log("je hebt E ingedrukt");
                 cam1.SetActive(false);
                 cam2.SetActive(true);
-                Dot.SetActive(false);
-                Cursor.lockState = CursorLockMode.None;
+                Cursor.lockState = ( CursorLockMode.None);
                 Cursor.visible = true;
+                Dot.SetActive(false);
             }
         }
         if (distanceToTExt >= radius)
